7022179d92695d127bbe2e8c687afde5f06fa687
romkalan <romkalan@mail.ru>
2023-07-14 12:50:34 +0300

    add player and movement with CoreMotion module

---
+++ WarBee/PlayerPlane.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerPlane.swift
+//  WarBee
+//
+//  Created by Roman Lantsov on 14.07.2023.
+//
+
+import SpriteKit
+
+class PlayerPlane: SKSpriteNode {
+    static func populate(at point: CGPoint) -> SKSpriteNode {
+        let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
+        let playerPlane = SKSpriteNode(texture: playerPlaneTexture)
+        playerPlane.setScale(0.5)
+        playerPlane.position = point
+        playerPlane.zPosition = 20
+        
+        return playerPlane
+    }
+}
