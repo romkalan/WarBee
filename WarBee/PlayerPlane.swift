0a7da9f7bb638f934bb6379c8fc29773da58ce9a
romkalan <romkalan@mail.ru>
2023-07-19 18:15:20 +0300

    correct code with for-in cycles in preloadTexture for animations turn

--- WarBee/PlayerPlane.swift
+++ WarBee/PlayerPlane.swift
@@ -26,6 +26,8 @@ class PlayerPlane: SKSpriteNode {
     var moveDirection: TurnDirection = .none
     var stillTurning = false
     
+    let animationSpriteStrides = [(13, 1, -1), (13, 26, 1), (13, 13, 1)]
+    
     static func populate(at point: CGPoint) -> PlayerPlane {
         let playerPlaneTexture = SKTexture(imageNamed: "airplane_3ver2_13")
         let playerPlane = PlayerPlane(texture: playerPlaneTexture)
@@ -47,8 +49,7 @@ class PlayerPlane: SKSpriteNode {
     }
     
     func performFly() {
-        planeAnimationFillArray()
-        
+        preloadTextureArrays()
         motionManager.accelerometerUpdateInterval = 0.2
         motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { [unowned self] (data, error) in
             guard let data = data else { return }
@@ -63,6 +64,32 @@ class PlayerPlane: SKSpriteNode {
         let planeSequence = SKAction.sequence([planeWaitAction, planeDirectionCheckAction])
         let planeSequenceForever = SKAction.repeatForever(planeSequence)
         self.run(planeSequenceForever)
+    }
+    
+    fileprivate func preloadTextureArrays() {
+        for i in 0...2 {
+            self.preloadArray(_stride: animationSpriteStrides[i]) { [unowned self] array in
+                switch i {
+                case 0: self.leftTextureArrayAnimation = array
+                case 1: self.rightTextureArrayAnimation = array
+                default: self.forwardTextureArrayAnimation = array
+                }
+            }
+        }
+    }
+    
+    fileprivate func preloadArray(_stride: (Int, Int, Int), callback: @escaping(_ array: [SKTexture]) -> Void) {
+        var array = [SKTexture]()
+        for i in stride(from: _stride.0, through: _stride.1, by: _stride.2) {
+            let number = String(format: "%02d", i)
+            let texture = SKTexture(imageNamed: "airplane_3ver2_\(number)")
+            array.append(texture)
+        }
+        
+        SKTexture.preload(array) {
+            callback(array)
+        }
+        
     }
     
     fileprivate func planeAnimationFillArray() {
