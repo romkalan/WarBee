bdf5042fbb67280b0f2fb7cc93f31a23876e73ea
romkalan <romkalan@mail.ru>
2023-07-19 14:09:34 +0300

    add rotationEffect for object in PowerUp class

--- WarBee/PowerUp.swift
+++ WarBee/PowerUp.swift
@@ -15,9 +15,29 @@ class PowerUp: SKSpriteNode {
     init() {
         let greenTexture = textureAtlas.textureNamed("missle_green_01")
         super.init(texture: greenTexture, color: .clear, size: initialSize)
+        self.name = "powerUp"
+        self.zPosition = 20
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func performRotation() {
+        for i in 1...15 {
+            let number = String(format: "%02d", i)
+            animationSpriteArray.append(SKTexture(imageNamed: "missle_green_\(number)"))
+        }
+        
+        SKTexture.preload(animationSpriteArray) {
+            let rotation = SKAction.animate(
+                with: self.animationSpriteArray,
+                timePerFrame: 0.05,
+                resize: true,
+                restore: false
+            )
+            let rotationForever = SKAction.repeatForever(rotation)
+            self.run(rotationForever)
+        }
     }
 }
