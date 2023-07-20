892eec80455171f15008494431c80e6686075f50
romkalan <romkalan@mail.ru>
2023-07-20 12:06:55 +0300

    add textureAtlas for Enemy and class Enemy

---
+++ WarBee/Enemy.swift
@@ -0,0 +1,29 @@
+//
+//  Enemy.swift
+//  WarBee
+//
+//  Created by Roman Lantsov on 20.07.2023.
+//
+
+import SpriteKit
+
+class Enemy: SKSpriteNode {
+    static var textureAtlas: SKTextureAtlas?
+    
+    init() {
+        let texture = Enemy.textureAtlas?.textureNamed("airplane_4ver2_13")
+        super.init(
+            texture: texture,
+            color: .clear,
+            size: CGSize(width: 221, height: 204)
+        )
+        self.xScale = 0.5
+        self.yScale = -0.5
+        self.zPosition = 20
+        self.name = "sprite"
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
