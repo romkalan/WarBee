849e493b445416256e23679af471bbd348630b29
romkalan <romkalan@mail.ru>
2023-07-23 12:03:10 +0300

    add menuScene for game and preload all textureAtlases

---
+++ WarBee/MenuScene.swift
@@ -0,0 +1,35 @@
+//
+//  MenuScene.swift
+//  WarBee
+//
+//  Created by Roman Lantsov on 23.07.2023.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+    override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
+        createButton()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let location = touches.first?.location(in: self) else { return }
+        let node = self.atPoint(location)
+        
+        if node.name == "runButton" {
+            let transition = SKTransition.crossFade(withDuration: 1.0)
+            let gameScene = GameScene(size: self.size)
+            gameScene.scaleMode = .aspectFill
+            self.scene?.view?.presentScene(gameScene, transition: transition)
+        }
+    }
+    
+    fileprivate func createButton() {
+        let texture = SKTexture(imageNamed: "play")
+        let button = SKSpriteNode(texture: texture)
+        button.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        button.name = "runButton"
+        self.addChild(button)
+    }
+}
