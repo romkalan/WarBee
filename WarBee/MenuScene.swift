19d2de49977994d2ef1f0e7bf03b413b7e2295ff
romkalan <romkalan@mail.ru>
2023-07-24 13:12:43 +0300

    move Assets.shared.preloadAssets() in MenuScene from AppDelegate

--- WarBee/MenuScene.swift
+++ WarBee/MenuScene.swift
@@ -9,6 +9,7 @@ import SpriteKit
 
 class MenuScene: SKScene {
     override func didMove(to view: SKView) {
+        Assets.shared.preloadAssets()
         self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
         createButton()
     }
