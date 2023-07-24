3bf66d9f79207ce7b09fa198335f1cdfd032d51e
romkalan <romkalan@mail.ru>
2023-07-24 14:58:06 +0300

    create HUD file for UserInterface configuration

--- WarBee/GameScene.swift
+++ WarBee/GameScene.swift
@@ -10,14 +10,9 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var player: PlayerPlane!
-    
-    let scoreBackground = SKSpriteNode(imageNamed: "scores")
-    let scoreLabel = SKLabelNode(text: "1000")
-    let menuButton = SKSpriteNode(imageNamed: "menu")
-    let life1 = SKSpriteNode(imageNamed: "life")
-    let life2 = SKSpriteNode(imageNamed: "life")
-    let life3 = SKSpriteNode(imageNamed: "life")
+    fileprivate var player: PlayerPlane!
+    fileprivate let hud = HUD()
+    fileprivate let screenSize = UIScreen.main.bounds.size
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
@@ -29,8 +24,7 @@ class GameScene: SKScene {
         player.performFly()
         spawnPowerUp()
         spawnEnemies()
-        
-        configureUI()
+        createHUD()
     }
     
     override func didSimulatePhysics() {
@@ -55,38 +49,9 @@ class GameScene: SKScene {
     }
     
     // Configure User Interface
-    fileprivate func configureUI() {
-        scoreBackground.position = CGPoint(
-            x: scoreBackground.size.width + 10,
-            y: self.size.height - scoreBackground.size.height / 2 - 30
-        )
-        scoreBackground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
-        scoreBackground.zPosition = 99
-        self.addChild(scoreBackground)
-        
-        scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.verticalAlignmentMode = .center
-        scoreLabel.position = CGPoint(x: -10, y: 3)
-        scoreLabel.zPosition = 100 // должен быть над scoreBackground
-        scoreLabel.fontName = "AmericanTypewriter-Bold"
-        scoreLabel.fontSize = 30
-        scoreBackground.addChild(scoreLabel)
-        
-        menuButton.position = CGPoint(x: 20, y: 20)
-        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-        menuButton.zPosition = 100
-        self.addChild(menuButton)
-        
-        let lifes = [life1, life2, life3]
-        for (index, life) in lifes.enumerated() {
-            life.position = CGPoint(
-                x: self.size.width - CGFloat(index + 1) * (life.size.width + 5),
-                y: self.size.height - 80
-            )
-            life.zPosition = 100
-            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-            self.addChild(life)
-        }
+    fileprivate func createHUD() {
+        self.addChild(hud)
+        hud.configureUI(screenSize: screenSize)
     }
     
     //Spawn playerFire
---
+++ WarBee/HUD.swift
@@ -0,0 +1,51 @@
+//
+//  HUD.swift
+//  WarBee
+//
+//  Created by Roman Lantsov on 24.07.2023.
+//
+
+import SpriteKit
+
+class HUD: SKScene {
+    let scoreBackground = SKSpriteNode(imageNamed: "scores")
+    let scoreLabel = SKLabelNode(text: "1000")
+    let menuButton = SKSpriteNode(imageNamed: "menu")
+    let life1 = SKSpriteNode(imageNamed: "life")
+    let life2 = SKSpriteNode(imageNamed: "life")
+    let life3 = SKSpriteNode(imageNamed: "life")
+    
+    func configureUI(screenSize: CGSize) {
+        scoreBackground.position = CGPoint(
+            x: scoreBackground.size.width + 10,
+            y: screenSize.height - scoreBackground.size.height / 2 - 30
+        )
+        scoreBackground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
+        scoreBackground.zPosition = 99
+        self.addChild(scoreBackground)
+        
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.verticalAlignmentMode = .center
+        scoreLabel.position = CGPoint(x: -10, y: 3)
+        scoreLabel.zPosition = 100 // должен быть над scoreBackground
+        scoreLabel.fontName = "AmericanTypewriter-Bold"
+        scoreLabel.fontSize = 30
+        scoreBackground.addChild(scoreLabel)
+        
+        menuButton.position = CGPoint(x: 20, y: 20)
+        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+        menuButton.zPosition = 100
+        self.addChild(menuButton)
+        
+        let lifes = [life1, life2, life3]
+        for (index, life) in lifes.enumerated() {
+            life.position = CGPoint(
+                x: screenSize.width - CGFloat(index + 1) * (life.size.width + 5),
+                y: screenSize.height - 80
+            )
+            life.zPosition = 100
+            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
+            self.addChild(life)
+        }
+    }
+}
